bda4e8441574bbb1395e56873f8000af817a4898
Almer <muhammadalmer2304@gmail.com>
2026-08-18 21:14:48 +0700

    go http server

---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main(){
+	listener, err := net.Listen("tcp", "1000")
+	if err != nil{
+		log.Fatal("Gagal terhubung ke TCP", err)
+	}
+
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil{
+			continue
+		}
+		fmt.Println("Server berhasil terhubung ke port 8080")
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn){
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+
+	requestLine, err := reader.ReadString('\n')
+	if err != nil{
+		return
+	}
+	fmt.Printf("Berhasil menerima request %s", requestLine)
+
+	parts := strings.Fields(requestLine)
+	method := parts[0]
+	path := parts[1]
+
+	if method != "GET"{
+		responseHandler(conn, "405 Method Not Allowed", "text/plain", []byte("Method Not Allowed"))
+		return
+	}
+
+	if path == "/"{
+		path = "/index.html"
+	}
+
+	filePath := filepath.Join("public")
+	fileData, err := os.ReadFile(filePath)
+	if err != nil{
+		fmt.Printf("File tidak ditemukan: %s", err)
+		responseHandler(conn, "404", "text/plain", []byte("<h1>404 Not Found</h1><p>File yang Anda cari tidak ada di server ini.</p>"))
+		return
+	}
+
+	contentType := getContentType(filePath)
+	responseHandler(conn, "200 OK", contentType, fileData)
+}
+
+func responseHandler(conn net.Conn, status string, contentType string, body []byte){
+	formatHeader := fmt.Sprintf(
+		"HTTP/1.1 %s\r\n"+
+		"Content-Type %s\r\n"+
+		"Content-Length %d\r\n"+
+		"Connection: close\r\n"+
+		"\r\n",
+		status,
+		contentType,
+		len(body),
+	)
+
+	conn.Write([]byte(formatHeader))
+	conn.Read(body)
+}
+
+func getContentType(contentType string) string{
+	ext := filepath.Ext(contentType)
+	switch ext{
+	case ".html":
+		return "text/html"
+	case ".css":
+		return "text/css"
+	case ".js":
+		return "application/javascript"
+	case ".json":
+		return "application/json"
+	case ".png":
+		return "image/png"
+	case ".jpg", ".jpeg":
+		return "image/jpeg"
+	default:
+		return "text/plain"
+
+	}
+}
